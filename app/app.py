d6bf2f1e532adf84309a2481f622564db75453eb
Sakthisundar Arivudainambi <sakthisundar2207@gmail.com>
2020-11-15 19:33:23 +0530

    Feature: Added app file

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from typing import List, Dict
+import mysql.connector
+import simplejson as json
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+def cities_import() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'citiesData'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM tblCitiesImport')
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    return result
+
+@app.route('/')
+def index() -> str:
+    js = json.dumps(cities_import())
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
